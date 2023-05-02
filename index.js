f9b1a21d84dd62f83d6aefcfb71d61789f2a998f
Matteo Emanuele Russo <matteoh8@hotmail.it>
2023-05-02 12:15:33 +0200

    git

--- index.js
+++ index.js
@@ -1,6 +1,59 @@
 //$ Costrutti condizionali
 
 //% Sintassi inline
-if (1 + 1 == 2) console.log("É vero");
+//if (1 + 1 == 2) console.log("É vero");
+// Output: É vero
 
 //% Sintassi tradizionale
+
+/* let number = 0;
+if (number > 0 ) {
+    console.log("Numero positivo");
+} else if (number == 0) {
+    console.log("Zero");
+} else {
+    console.log("Numero negativo");
+} // Output: Zero */
+
+//% Ternary Operator
+
+/* const number = -2;
+const result = (number >= 0) ? "Positive" : "Negative";
+console.log(result); //Negative */
+
+/* const number = -2;
+const result = number > 0 ? "Positive" : number == 0 ? "Zero" : "Negative";
+console.log(result); //Negative */
+
+//% Switch Case
+
+let giorno = 3;
+let nomeGiorno;
+
+switch (giorno) {
+  case 1:
+    nomeGiorno = "Lunedì";
+    break;
+  case 2:
+    nomeGiorno = "Martedì";
+    break;
+  case 3:
+    nomeGiorno = "Mercoledì";
+    break;
+  case 4:
+    nomeGiorno = "Giovedì";
+    break;
+  case 5:
+    nomeGiorno = "Venerdì";
+    break;
+  case 6:
+    nomeGiorno = "Sabato";
+    break;
+  case 7:
+    nomeGiorno = "Domenica";
+    break;
+  default:
+    nomeGiorno = "Giorno non valido";
+}
+
+console.log(nomeGiorno); // Output: "Mercoledì"
