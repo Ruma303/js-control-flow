d42838b93df54c0148f7e55fac32b0e6b534bb9f
Matteo Emanuele Russo <matteoh8@hotmail.it>
2023-05-06 18:53:11 +0200

    modifica combo operatori e costrutti

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 //$ Costrutti condizionali
 
 //! Block Scope
-/* let sum = null, a = 5, b = 10;
+/*
+let sum = null, a = 5, b = 10;
 
-if(a > 3  &&   b != 12) {
-  sum = a + b;
+if(a > 3  &&  b != 12) {
+  sum = a + b; //15
 }
-console.log(sum); */
-
+console.log(sum);
+ */
 /* let sum = null, a = 5, b = 10;
 let string = "Sono visibile perché sono nel contesto globale"
 
-if(a > 3  &&   b != 12) {
-  sum = a + b;
+//console.log(b);
+if(a > 3 && b != 12) {
+    sum = a + b;
+  //console.log(string);
   let string = "Non sono visibile fuori dal blocco"
+  //console.log(string);
 }
-console.log(string); */
+//console.log(string);
+console.log(sum); */
+
 
 //% Sintassi inline
 
@@ -25,7 +31,7 @@ console.log(string); */
 //% Sintassi tradizionale
 
 /* let number = 0;
-if (number > 0 ) {
+if (number > 0) {
     console.log("Numero positivo");
 } else if (number == 0) {
     console.log("Zero");
@@ -35,17 +41,17 @@ if (number > 0 ) {
 
 //% Ternary Operator
 
-/* const number = -2;
-const result = (number >= 0) ? "Positive" : "Negative";
+//const number = -2;
+/* const result = (number >= 0) ? "Positive" : "Negative";
 console.log(result); //Negative */
 
-/* const number = -2;
-const result = number > 0 ? "Positive" : number == 0 ? "Zero" : "Negative";
-console.log(result); //Negative */
+//const number = -2;
+/* const result = (number > 0) ? "Positive" : (number == 0) ? "Zero" : "Negative";
+console.log(result); */ //Negative
 
 //% Switch Case
-/*
-let giorno = 3;
+
+/* let giorno = 3;
 let nomeGiorno;
 
 switch (giorno) {
@@ -73,8 +79,8 @@ switch (giorno) {
   default:
     nomeGiorno = "Giorno non valido";
 }
-console.log(nomeGiorno); // Output: "Mercoledì"
- */
+console.log(nomeGiorno); // Output: "Mercoledì" */
+
 
 
 //$ Costrutti Ciclici
@@ -83,11 +89,12 @@ console.log(nomeGiorno); // Output: "Mercoledì"
 
 /* for (let i = 1; i <= 5; i++) {
   console.log(`Istruzione numero ${i}`);
-} */
+}
+ */
 
 //% break
-/*
-for (let i = 1; i <= 5; i++) {
+
+/* for (let i = 1; i <= 5; i++) {
   if(i === 3) {
     break;
   }
@@ -95,14 +102,14 @@ for (let i = 1; i <= 5; i++) {
 } */
 
 //% continue
-
-/* for (let i = 1; i <= 5; i++) {
+/*
+for (let i = 1; i <= 5; i++) {
   if(i === 3) {
     continue;
   }
   console.log(`Istruzione numero ${i}`);
-} */
-
+}
+ */
 
 //% Ciclo while
 /* let number = 1;
@@ -112,42 +119,60 @@ while(number <=5){
 } */
 
 //% Ciclo do while
-/* let number = 1;
+/* let number = 30000000000;
 do {
   console.log(`Istruzione numero ${number}`);
   number++;
-} while (number <=5) */
-
+} while (number <=5)
+*/
 
 //% Ciclo foreach
 
 //* con un parametro
-/* let array = ["Marco", "Paolo", "Sara", "Ester"];
-array.forEach(function(element) {
-  console.log(`Ciao ${element}`)
+//let array = ["Marco", "Paolo", "Sara", "Ester"];
+/* array.forEach(function(element) {
+  console.log(`Ciao ${element}`);
 }); */
 
 //* con due parametri
-/* array.forEach(function(element, index) {
-  console.log(`Ciao ${element} con id: ${index}`)
-}); */
-
+/* array.forEach(function(element, pippo) {
+  console.log(`Ciao ${element} con id: ${pippo+10}`)
+});
+ */
 //* con arrow function
-/*
-let array = [1,2,3,4,5];
-array.forEach(element => {
-  console.log(`Istruzione numero ${element}`)
-}); */
+
+//let array = [1,2,3,4,5];
+/* array.forEach((element, index) => {
+  console.log(`Ciao ${element} con id: ${index+1}`)
+});
+*/
 
 //% Ciclo for in
+//! Sconsigliato
 
 /* let array = ["Marco", "Paolo", "Sara", "Ester"];
+//let array = [0,1,2,3,4];
 for (nome in array) {
+  console.log(typeof nome);
   console.log(`Nome: ${array[nome]}, posizione array: ${nome}`);
 } */
 
-//% Ciclo for in
-/* let array = [1,2,3,4];
-for (let number of array) {
-  console.log(`Posizione elemento: ${number}`);
+//% Ciclo for of
+/* let array = ["Marco", "Paolo", "Sara", "Ester"];
+//let array = [1,2,3,4];
+for (let pippo of array) {
+  console.log(`Posizione elemento: ${pippo}`);
 } */
+
+/* let a = 10, b = "ciao";
+const result = ((a >= 10 && a < 30) &&
+(b == null || b == "saluta")) ?
+"tutto vero" : "tutto falso";
+
+console.log(result); //tutto falso */
+
+//(true && true) && (false || false)
+//true && false
+//false
+
+
